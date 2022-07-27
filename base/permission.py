8575f6d719bc0b8bdb72dcf1498e4080c33a1fea
sabbir1021 <5boysbd@hmail.com>
2022-07-28 00:34:27 +0600

    deposit validation and balance add

--- base/permission.py
+++ base/permission.py
@@ -3,7 +3,7 @@ from agent.models import Agent
 from django.http import Http404
 from django.contrib.auth import get_user_model
 User= get_user_model()
-from payment.models import PaymentMethod
+from payment.models import PaymentMethod, Deposit
 
 
 
@@ -70,25 +70,37 @@ class DepositPermission(BasePermission):
             if request.data.get('agent'):
                 try:
                     agent = Agent.objects.get(id=request.data['agent'])
-                    if agent.country == request.user.agent.country:
+                    if agent.agent_type == "local_agent":
+                        if agent.country == request.user.agent.country:
+                            request.data['status'] = "approved"
+                            return True
+                        else:
+                            return False
+                    else:
                         return False
                 except:
-                    pass
+                    raise Http404
+            else:
+                if request.user.user_type == "local_user":
+                    request.data['agent'] = user.agent.id
+                    request.data['status'] = "pending"
+                    return True
+                else:
+                    return False
+
+class DepositUpdatePermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method =='GET':
+            deposit = Deposit.objects.get(id=view.kwargs.get('pk'))
+            if deposit.agent.country == request.user.agent.country and request.user.user_type == "country_user":
+                return True
+            if deposit.agent == request.user.agent:
+                return True
+            return False
+        if request.method =='PATCH':
+            user = request.user
+            deposit = Deposit.objects.get(id=view.kwargs.get('pk'))
+            if user.user_type == "country_user" and deposit.agent.country == request.user.agent.country:
+                return True
             else:
                 return False
-
-            # try:
-            #     agent = Agent.objects.get(id=request.data['agent'])
-            #     if agent.country == request.user.agent.country:
-            #         if user.user_type == "country_user":
-            #             request.data['status'] = "approved"
-            #             return True
-            #         if agent == user.agent:
-            #             request.data['agent'] = 
-            #             request.data['status'] = "pending"
-            #             return True
-            #         return False
-            #     else:
-            #         return False
-            # except PaymentMethod.DoesNotExist:
-            #     raise Http404
--- payment/views.py
+++ payment/views.py
@@ -9,7 +9,7 @@ User= get_user_model()
 from .serializers import PaymentMethodSerializer, DepositSerializer
 from django.http import Http404
 from payment.models import PaymentMethod, Deposit, DepositHistory
-from base.permission import PaymentMethodPermission,DepositPermission
+from base.permission import PaymentMethodPermission, DepositPermission, DepositUpdatePermission
 from datetime import datetime
 from agent.models import Agent
 # Create your views here.
@@ -48,25 +48,28 @@ class PaymentMethodViewUpdate(APIView):
 
 
 class DepositListCreate(APIView):
-    permission_classes = [IsAuthenticated]
-
+    permission_classes = [IsAuthenticated, DepositPermission]
     def get(self, request, format=None):
         deposits = Deposit.objects.all()
         serializer = DepositSerializer(deposits, many=True)
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        print(request.data)
         serializer = DepositSerializer(data=request.data)
         if serializer.is_valid():
             obj = serializer.save()
             DepositHistory.objects.create(deposit=obj,status_name=request.data['status'],date_time=datetime.now())
+            if request.data['status'] == "approved":
+                agent = Agent.objects.get(id=request.data['agent'])
+                print(agent.balance,request.data['amount'])
+                agent.balance = (0 if agent.balance == None else agent.balance) + request.data['amount']
+                agent.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DepositViewUpdate(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, DepositUpdatePermission]
     def get(self, request, pk):
         try:
             snippet = Deposit.objects.get(pk=pk)
@@ -81,9 +84,18 @@ class DepositViewUpdate(APIView):
         except:
             raise Http404
         status = request.data['status']
+        previous_status = snippet.status
         serializer = DepositSerializer(snippet, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             DepositHistory.objects.create(deposit=snippet,status_name=status,date_time=datetime.now())
+            if previous_status != "approved" and status == "approved":
+                agent = snippet.agent
+                agent.balance = (0 if agent.balance == None else agent.balance) + snippet.amount
+                agent.save()
+            if previous_status == "approved" and status == "rejected":
+                agent = snippet.agent
+                agent.balance = (0 if agent.balance == None else agent.balance) - snippet.amount
+                agent.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
